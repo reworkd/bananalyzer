7aeea213b5dc5b488ec195e80fb5a664c234945f
asim-shrestha <asim.shrestha@hotmail.com>
2023-11-03 13:25:41 -0700

    🍌 Agent Wrapper

---
+++ python/bananalyzer/runner/agent_wrapper.py
@@ -0,0 +1,22 @@
+from abc import abstractmethod, ABC
+from typing import Dict, Union, Any, List
+
+from playwright.async_api import BrowserContext
+
+from bananalyzer.data.schemas import Example
+
+AgentResult = Union[Dict[str, Any], List[Dict[str, Any]], str]
+
+
+class AgentWrapper(ABC):
+    """
+    Wrapper class clients must implement to run an agent against the evaluations
+    """
+
+    @abstractmethod
+    def run(
+        self,
+        browser_context: BrowserContext,
+        eval_context: Example,
+    ) -> AgentResult:
+        pass
---
+++ python/tests/runner/test_runner.py
@@ -0,0 +1,5 @@
+from bananalyzer.runner.runner import run_tests
+
+
+def test_run_tests() -> None:
+    run_tests()
