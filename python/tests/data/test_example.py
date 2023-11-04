37b4a534dfca2396cd6a0a3e573392558c766a59
asim-shrestha <asim.shrestha@hotmail.com>
2023-11-04 13:58:47 -0700

    🍌 Test example

--- python/tests/data/test_example.py
+++ python/tests/data/test_example.py
@@ -1,4 +1,10 @@
-from bananalyzer.data.schemas import JSONEval
+from typing import Dict, Any, Optional
+
+import pytest
+from pydantic import ValidationError
+
+from bananalyzer.data.fetch_schemas import fetch_schemas
+from bananalyzer.data.schemas import JSONEval, Example
 
 
 def test_json_eval() -> None:
@@ -9,3 +15,51 @@ def test_json_eval() -> None:
     assert eval.eval_results(json)
     assert eval.eval_results({"two": "two", "one": "one"})
     assert not eval.eval_results({"test": "test"})
+
+
+def create_default_example(
+    overrides: Optional[Dict[str, Any]] = None
+) -> Dict[str, Any]:
+    default_example: Dict[str, Any] = {
+        "id": "1",
+        "url": "url",
+        "source": "mhtml",
+        "domain": "domain",
+        "subdomain": "subdomain",
+        "type": "fetch",
+        "evals": [],
+        "fetch_id": None,  # Set as None by default
+        "goal": None,  # Set as None by default
+    }
+    if overrides:
+        default_example.update(overrides)
+    return default_example
+
+
+def test_non_fetch_with_goal() -> None:
+    example_data = create_default_example({"type": "multiple", "goal": "goal"})
+    Example(**example_data)
+
+
+def test_fetch_without_fetch_id_exception() -> None:
+    example_data = create_default_example({"type": "fetch", "fetch_id": None})
+    # Since fetch_id is None by default, no need to override it
+    with pytest.raises(Exception):
+        Example(**example_data)
+
+
+def test_fetch_with_fetch_id_and_goal_should_raise_validation_error() -> None:
+    example_data = create_default_example(
+        {
+            "fetch_id": "job_posting",
+            "goal": "goal",
+        }
+    )
+    with pytest.raises(ValidationError):
+        Example(**example_data)
+
+
+def test_fetch_with_fetch_id_and_no_goal_sets_default_goal() -> None:
+    example_data = create_default_example({"fetch_id": "job_posting", "goal": None})
+    example = Example(**example_data)
+    assert example.goal == fetch_schemas["job_posting"]
