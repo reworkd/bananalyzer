5181cfb4a267f78d6a5a3a6b750ccde065ea7d1d
asim-shrestha <asim.shrestha@hotmail.com>
2023-11-05 20:44:55 -0800

    🍌 Update wording

--- cli-test/cli_test/__init__.py
+++ cli-test/cli_test/__init__.py
@@ -1,5 +1,5 @@
 """
-Test bananalyzer.py file to ensure CLI is working as expected within the project
+Test bananalyzer_agent.py file to ensure CLI is working as expected within the project
 """
 from bananalyzer.runner.null_agent_wrapper import NullAgentRunner
 
