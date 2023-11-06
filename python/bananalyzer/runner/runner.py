4ca35caa9f7df4031d1e1cdebfd3f88780375a2b
asim-shrestha <asim.shrestha@hotmail.com>
2023-11-05 20:12:01 -0800

    🍌 Update runner to load agent import and clean up files

--- python/bananalyzer/runner/runner.py
+++ python/bananalyzer/runner/runner.py
@@ -1,4 +1,6 @@
 # dynamic_test_runner.py
+import os
+import tempfile
 from typing import Callable, Awaitable, List
 
 import pytest
@@ -13,31 +15,58 @@ def generate_test(example: Example) -> str:
 @pytest.mark.asyncio
 async def test_{example.id.replace("-", "_")}() -> None:
     async with async_playwright() as p:
-        browser = await p.chromium.launch()
+        browser = await p.chromium.launch(headless=False)
         context = await browser.new_context()
-        agent = AgentWrapper()
-        example = get_example_by_url("{example.url}") 
-        result = agent.run(context, example)
+        example = get_example_by_url("{example.url}")
+         
+        # The agent is imported into the global context prior to this call 
+        result = await agent.run(context, example)
         for curr_eval in example.evals:
             assert curr_eval.eval_results(result)
 """
 
 
-def run_tests(tests: List[str]) -> int:
+def write_load_agent_module(f, file_path: str) -> None:
+    f.write(
+        f"""
+import importlib.util
+import sys
+from pathlib import Path
+
+def load_agent_module():
+    path = Path("{file_path}")
+    module_name = path.stem
+
+    spec = importlib.util.spec_from_file_location(module_name, "{file_path}")
+    module = importlib.util.module_from_spec(spec)
+    sys.modules[module_name] = module
+    spec.loader.exec_module(module)
+    globals()["agent"] = getattr(module, "agent")
+load_agent_module()
+"""
+    )
+
+
+def run_tests(tests: List[str], agent_file_path: str) -> int:
     """
-    Create temporary test file and run it
+    Create temporary test file, run it, and then delete it
     """
-    test_file_name = "dynamic_tests.py"
-    with open(test_file_name, "w") as f:
-        # Write necessary imports at the top of the file
+    with tempfile.NamedTemporaryFile(mode="w+", delete=False, suffix=".py") as f:
         f.write("import pytest\n")
         f.write("from bananalyzer.data.examples import get_example_by_url\n")
-        f.write("from bananalyzer.runner.agent_runner import AgentWrapper\n")
         f.write("from playwright.async_api import async_playwright\n\n")
+        f.write("global test\n")
+        f.write("test = 'test'\n")
+        write_load_agent_module(f, agent_file_path)
 
-        # Write each test to the file
-        for test in tests:
-            f.write(f"{test}\n\n")
+        for test_content in tests:
+            f.write(f"{test_content}\n\n")
 
-    # Now that all tests are written to the file, run them with pytest
-    return pytest.main([test_file_name])
+        test_file_name = f.name
+
+    try:
+        return_code = pytest.main([test_file_name])
+    finally:
+        os.unlink(test_file_name)
+
+    return return_code
