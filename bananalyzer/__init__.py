b534a8be5e2270b9694ac28024807f5a1ce00b79
awtkns <32209255+awtkns@users.noreply.github.com>
2023-11-21 20:18:47 -0800

    ✅ fix main

--- bananalyzer/__init__.py
+++ bananalyzer/__init__.py
@@ -1,5 +1,6 @@
 from bananalyzer.data.examples import examples
 from bananalyzer.data.schemas import Example, GoalType
 from bananalyzer.runner.agent_runner import AgentRunner
+from bananalyzer.__main__ import main
 
-__all__ = ["AgentRunner", "GoalType", "Example", "examples"]
+__all__ = ["AgentRunner", "GoalType", "Example", "examples", "main"]
