8bd91ae770fa4e5e54072fa6672d8e45b12260ea
asim-shrestha <asim.shrestha@hotmail.com>
2023-11-30 13:39:40 -0800

    ✨ Update check_match

--- bananalyzer/__init__.py
+++ bananalyzer/__init__.py
@@ -1,5 +1,6 @@
 from bananalyzer.data.examples import get_all_examples
 from bananalyzer.data.schemas import Example, GoalType
 from bananalyzer.runner.agent_runner import AgentRunner
+from bananalyzer.runner.evals import check_match
 
-__all__ = ["AgentRunner", "GoalType", "Example", "get_all_examples"]
+__all__ = ["AgentRunner", "GoalType", "Example", "get_all_examples", "check_match"]
