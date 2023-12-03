b14af21aeae53779807c421e8b6f09799dddc383
awtkns <32209255+awtkns@users.noreply.github.com>
2023-12-02 16:24:06 -0800

    scaffold

--- next/src/app/page.tsx
+++ next/src/app/page.tsx
@@ -1,7 +1,8 @@
 import { CreatePost } from "~/app/_components/create-post";
 import { getServerAuthSession } from "~/server/auth";
 import { api } from "~/trpc/server";
-import {db} from "~/server/db";
+import { db } from "~/server/db";
+import { LineChart } from "@tremor/react";
 
 export default async function Home() {
   // const hello = await api.post.hello.query({ text: "from tRPC" });
@@ -16,18 +17,29 @@ export default async function Home() {
 }
 
 async function SuiteGrid() {
-  "use server"
-
+  "use server";
 
   const suites = await db.testSuite.findMany();
 
   return (
-    <div className="grid grid-cols-2">
-      {suites.map((suite) => (
-        <div key={suite.id}>
-          <div>{suite.name}</div>
-        </div>
-      ))}
-    </div>
+    <>
+      {/*<LineChart*/}
+      {/*    className="mt-6"*/}
+      {/*    data={chartdata}*/}
+      {/*    index="year"*/}
+      {/*    categories={["Export Growth Rate", "Import Growth Rate"]}*/}
+      {/*    colors={["emerald", "gray"]}*/}
+      {/*    // valueFormatter={valueFormatter}*/}
+      {/*    yAxisWidth={40}*/}
+      {/*/>*/}
+
+      <div className="grid grid-cols-3">
+        {suites.map((suite) => (
+          <div key={suite.id}>
+            <div>{suite.name}</div>
+          </div>
+        ))}
+      </div>
+    </>
   );
 }
