8d29a3101535b4303695695f6d1f0a3ca16b5025
asim-shrestha <asim.shrestha@hotmail.com>
2023-12-02 19:00:09 -0800

    ✨ Remove hard coded data

--- next/src/app/_components/test-suites-table.tsx
+++ next/src/app/_components/test-suites-table.tsx
@@ -1,3 +1,6 @@
+"use server";
+import { type TestSuite } from "@prisma/client";
+import { db } from "~/server/db";
 import {
   Badge,
   Button,
@@ -10,86 +13,11 @@ import {
   TableHeaderCell,
   TableRow,
   Text,
-  Title,
+  Title
 } from "@tremor/react";
-import { db } from "~/server/db";
-
-const colors = {
-  "Ready for dispatch": "gray",
-  Cancelled: "rose",
-  Shipped: "emerald",
-};
-
-const transactions = [
-  {
-    transactionID: "#123456",
-    user: "Lena Mayer",
-    item: "Under Armour Shorts",
-    status: "Ready for dispatch",
-    amount: "$ 49.90",
-    link: "#",
-  },
-  {
-    transactionID: "#234567",
-    user: "Max Smith",
-    item: "Book - Wealth of Nations",
-    status: "Ready for dispatch",
-    amount: "$ 19.90",
-    link: "#",
-  },
-  {
-    transactionID: "#345678",
-    user: "Anna Stone",
-    item: "Garmin Forerunner 945",
-    status: "Cancelled",
-    amount: "$ 499.90",
-    link: "#",
-  },
-  {
-    transactionID: "#4567890",
-    user: "Truls Cumbersome",
-    item: "Running Backpack",
-    status: "Shipped",
-    amount: "$ 89.90",
-    link: "#",
-  },
-  {
-    transactionID: "#5678901",
-    user: "Peter Pikser",
-    item: "Rolex Submariner Replica",
-    status: "Cancelled",
-    amount: "$ 299.90",
-    link: "#",
-  },
-  {
-    transactionID: "#6789012",
-    user: "Phlipp Forest",
-    item: "On Clouds Shoes",
-    status: "Ready for dispatch",
-    amount: "$ 290.90",
-    link: "#",
-  },
-  {
-    transactionID: "#78901234",
-    user: "Mara Pacemaker",
-    item: "Ortovox Backpack 40l",
-    status: "Shipped",
-    amount: "$ 150.00",
-    link: "#",
-  },
-  {
-    transactionID: "#89012345",
-    user: "Sev Major",
-    item: "Oakley Jawbreaker",
-    status: "Ready for dispatch",
-    amount: "$ 190.90",
-    link: "#",
-  },
-];
 
 export default async function TestSuitesTable() {
-  "use server";
-  const suites = await db.testSuite.findMany();
+  const suites: TestSuite[] = await db.testSuite.findMany();
 
   return (
     <Card>
