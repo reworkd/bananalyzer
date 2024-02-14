b511b431fc544b1a32bc355de7de8c2807b7629a
asim-shrestha <asim.shrestha@hotmail.com>
2024-02-14 11:47:49 -0800

    ✨ Update user agent

--- bananalyzer/runner/runner.py
+++ bananalyzer/runner/runner.py
@@ -79,7 +79,11 @@ def agent():
 async def page():
     async with async_playwright() as p:
         browser = await p.chromium.launch(headless={headless})
-        context = await browser.new_context(viewport={{ 'width': 1280, 'height': 1024 }})
+        context = await browser.new_context(
+            viewport={{ 'width': 1280, 'height': 1024 }},
+            ignore_https_errors=True,
+            user_agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36",
+        )
         page = await context.new_page()
         await stealth_async(page)
         yield page
