35ea6cf8a41efc45659cffa43dbc41d8b7da77e7
Sunshine <snshn@tutanota.com>
2024-07-31 01:35:18 +0000

    lift limit on creating HAR files, do not index bananas.har

--- static/re-sync-har.py
+++ static/re-sync-har.py
@@ -13,33 +13,29 @@ Test script to re-load websites and create HAR cache files.
 
 async def run_examples_and_create_cache(examples: List[Example]) -> None:
     async with async_playwright() as p:
-        limit = 5
-        for item in examples:
-            # Only work with limited amount of examples
-            if limit < 1:
-                break
-
+        for i, item in enumerate(examples):
             try:
                 browser = await p.chromium.launch(headless=True)
                 context = await browser.new_context()
                 page = await context.new_page()
                 page.set_default_timeout(100000)
                 client = await context.new_cdp_session(page)
-                print(item.id, item.url)
+
+                print(f"[{i + 1}/{len(examples)}]", item.id, item.url)
                 folder_path: str = f"examples/{item.id}"
                 folder_path_abs: str = os.path.join(os.path.dirname(os.path.abspath(__file__)), folder_path)
                 os.makedirs(folder_path_abs, exist_ok=True)
                 file_path: str = os.path.join(folder_path_abs, "bananas.har")
 
-                await page.route_from_har(file_path, not_found="fallback", update=True)
+                await page.route_from_har(file_path, not_found="fallback", update=True, update_content="embed")
                 await page.goto(item.url)
+                # TODO: crawl the site using agent
 
                 await context.close()
                 await browser.close()
             except Exception as e:
                 print(e)
                 continue
-            limit = limit - 1
 
 
 if __name__ == "__main__":
